bd3fc1a52c207df0d4d0a38408df7d00b2f84e2d
ValorWind1 <skyjdani@gmail.com>
2019-05-08 21:39:04 -0400

    intro

---
+++ intro.py
@@ -0,0 +1,11 @@
+print("Hello there")
+print("Nice to meet you what is your name ? ")
+
+User_Name = input()
+
+print("I hope you're having a great day ! " + User_Name)
+print(" By the way neat fact, The length of your name is : ")
+print(len(User_Name))
+print(" what would be your age ? " + User_Name)
+User_Age = input ()
+print ( " Look how hold you will become in 20 years :(anxiety rises)" +str(int(User_Age)+20))
